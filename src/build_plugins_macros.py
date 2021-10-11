2358bbff7a2b759d9ea2a79a78f31b5ecef00a4f
manoch-iris <manoch@iris.washington.edu>
2021-10-11 07:40:46 -0400

    r2 Python 3

--- src/build_plugins_macros.py
+++ src/build_plugins_macros.py
@@ -1,55 +1,55 @@
-# Credits:
-#
-#  This code closely follows build_plugins.sh script developed by  Bane Sullivan (http://banesullivan.com/)
-#  see:
-#       https://github.com/banesullivan/ParaViewGeophysics
-#
-# Description:
-#
-# This script builds all server manager XML plugins .py macros from the .py files that describe plugins and macros.
-# There are 3 stages to this process:
-#    1. build the readers
-#    2. build the filters
-#    3. update the macros
-#
-# NOTE: plugins and macros are created from files with specific prefixex as follows:
-# 
-#           Filters     ->      './filters/filter_*.py'
-#           Readers     ->      './readers/read_*.py'
-#           Macros      ->      './macros/macro_*.py'
-#
-# History:
-#
-#     2018-03-21 Manoch:  R.0.2018.080
-#
 import sys
 import os
-import subprocess 
+import subprocess
 import IrisEMC_Paraview_Param as param
 import IrisEMC_Paraview_Utils as utils
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+"""
+ Credits:
 
-################################################################################################
-#
-# fileName
-#
-################################################################################################
-def fileName(fullFileName):
+  This code closely follows build_plugins.sh script developed by  Bane Sullivan (http://banesullivan.com/)
+  see:
+       https://github.com/banesullivan/ParaViewGeophysics
+
+ Description:
+
+ This script builds all server manager XML plugins .py macros from the .py files that describe plugins and macros.
+ There are 3 stages to this process:
+    1. build the readers
+    2. build the filters
+    3. update the macros
+
+ NOTE: plugins and macros are created from files with specific prefixex as follows:
+
+           Filters     ->      './filters/filter_*.py'
+           Readers     ->      './readers/read_*.py'
+           Macros      ->      './macros/macro_*.py'
+
+ History:
+     2021-10-03 Manoch: v.2021.276 Python 3 r2
+     2018-03-21 Manoch: v.2018.080
+"""
+
+
+class bcolors:
+    def __init__(self):
+        self.HEADER = '\033[95m'
+        self.OKBLUE = '\033[94m'
+        self.OKGREEN = '\033[92m'
+        self.WARNING = '\033[93m'
+        self.FAIL = '\033[91m'
+        self.ENDC = '\033[0m'
+        self.BOLD = '\033[1m'
+        self.UNDERLINE = '\033[4m'
+
+
+def extract_file_name(full_file_name):
     """
     for a given full file name, extract and return a file name without extension and path
 
     Parameters
     ----------
-    fullFileName: str
+    full_file_name: str
        a full file name
 
     Returns
@@ -57,67 +57,63 @@ def fileName(fullFileName):
     filename: str
        file name without extension and path
     """
-    filename_w_ext = os.path.basename(fullFileName)
-    filename, file_extension = os.path.splitext(filename_w_ext)
-    return filename
+    file_name_w_ext = os.path.basename(full_file_name)
+    file_name, file_extension = os.path.splitext(file_name_w_ext)
+    return file_name
 
-print "\n\n installing under %s OS\n\n" % utils.check_system()
-#
-#  wrap READERS in XML
-#
-print "\n\n.... Attempting to wrap READERS in XML...\n"
-for file in os.listdir("readers"):
-    if not file.startswith("read_"):
-        print '**** WARNING ....skipped',file
+
+print(f"\n\n[INFO] installing under {utils.check_system()} OS\n\n")
+
+#  Wrap READERS in XML.
+print("\n\n[INFO] .... Attempting to wrap READERS in XML...\n")
+for _file in os.listdir("readers"):
+    if not _file.startswith("read_"):
+        print(f'[WARN] ....skipped file: {_file}')
         continue
-    print '....trying',file
+    print(f'[INFO] ....trying file: {_file}')
 
-    inputFile = os.path.join('readers',file)
-    filename = fileName(file)
-    outputFile = os.path.join(param.pathDict['EMC_PLUGINS_PATH'],filename+'.xml')
+    input_file = os.path.join('readers', _file)
+    filename = extract_file_name(_file)
+    output_file = os.path.join(param.pathDict['EMC_PLUGINS_PATH'], f'{filename}.xml')
     try:
-      subprocess.check_output([sys.executable ,"python_filter_generator.py",inputFile,outputFile])
-      print "++++ SUCCESS:",inputFile,"--->",outputFile
-    except:
-      print "---- FAILED:",inputFile,"--->",outputFile
-      pass
+        subprocess.check_output([sys.executable, "python_filter_generator.py", input_file, output_file])
+        print(f"[INFO] SUCCESS: {input_file} ---> {output_file}")
+    except Exception as ex:
+        print(f"[ERR] FAILED: {input_file} ---> {output_file}\n{ex}")
+        pass
 
-#
-#  wrap FILTERS in XML
-#
-print "\n\n.... Attempting to wrap FILTERS in XML...\n"
-for file in os.listdir("filters"):
-    if not file.startswith("filter_"):
-        print '**** WARNING ....skipped',file
+#  Wrap FILTERS in XML.
+print("\n\n[INFO] .... Attempting to wrap FILTERS in XML...\n")
+for _file in os.listdir("filters"):
+    if not _file.startswith("filter_"):
+        print(f'[WARN] ....skipped file: {_file}')
         continue
-    print '....trying',file
+    print(f'[INFO]....trying file: {_file}')
 
-    inputFile = os.path.join('filters',file)
-    filename = fileName(file)
-    outputFile = os.path.join(param.pathDict['EMC_PLUGINS_PATH'],filename+'.xml')
+    input_file = os.path.join('filters', _file)
+    filename = extract_file_name(_file)
+    output_file = os.path.join(param.pathDict['EMC_PLUGINS_PATH'], f"{filename}.xml")
     try:
-      subprocess.check_output([sys.executable ,"python_filter_generator.py",inputFile,outputFile])
-      print "++++ SUCCESS:",inputFile,"--->",outputFile
-    except:
-      print "---- FAILED:",inputFile,"--->",outputFile
-      pass
+        subprocess.check_output([sys.executable, "python_filter_generator.py", input_file, output_file])
+        print(f"[INFO] SUCCESS: {input_file} ---> {output_file}")
+    except Exception as ex:
+        print(f"[ERR] FAILED: {input_file} ---> {output_file}\n{ex}")
+        pass
 
-#
-#  update MACROS
-#
-print "\n\n.... Attempting to update MACROS ...\n"
-for file in os.listdir("macros"):
-    if not file.startswith("macro_"):
-        print '**** WARNING ....skipped',file
+#  Update MACRO.
+print(f"\n\n[INFO].... Attempting to update MACROS ...\n")
+for _file in os.listdir("macros"):
+    if not _file.startswith("macro_"):
+        print(f'[WARN] ....skipped file:{_file}')
         continue
-    print '....trying',file
+    print(f'[INFO] ....trying file: {_file}')
 
-    inputFile = os.path.join('macros',file)
-    filename = fileName(file)
-    outputFile = os.path.join('..','macros',filename.replace('macro_','')+'.py')
+    input_file = os.path.join('macros', _file)
+    filename = extract_file_name(_file)
+    output_file = os.path.join('..', 'macros', f"{filename.replace('macro_', '')}.py")
     try:
-      subprocess.check_output([sys.executable ,"python_macro_generator.py",inputFile,outputFile])
-      print "++++ SUCCESS:",inputFile,"--->",outputFile
-    except:
-      print "---- FAILED:",inputFile,"--->",outputFile
-      pass
+        subprocess.check_output([sys.executable, "python_macro_generator.py", input_file, output_file])
+        print(f"[INFO] SUCCESS: {input_file} ---> {output_file}")
+    except Exception as ex:
+        print(f"[ERR] FAILED: {input_file} ---> {output_file}\n{ex}")
+        pass
